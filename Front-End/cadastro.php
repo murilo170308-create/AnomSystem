6181e62093937f5f87ba7aa1b5ea08914120a901
murilo170308-create <murilo170308@gmail.com>
2025-11-04 16:40:19 -0300

    cartões de cadastros feitos

--- Front-End/cadastro.php
+++ Front-End/cadastro.php
@@ -1,14 +1,62 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/styleCadastro.css">
+  <title>AnomSystem - Cadastros</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+
+  <!-- CSS próprio -->
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="responsáveis">
-        <h3>Responsáveis</h3>
-        <p>Cadastros de responsáveis que iram ser incluídos na anomalia</p>
+
+  <!-- TOPNAV -->
+  <div class="navbar">
+    <h1>Cadastros</h1>
+    <button type="button" class="btn btn-danger">Voltar</button>
+  </div>
+
+  <!-- CONTAINER DOS CARDS -->
+  <div class="container">
+    <div class="card">
+      <div class="card-text">
+        <h2>Responsáveis</h2>
+        <p>Cadastros de Responsáveis que irão ser incluídos na Anomalia</p>
+      </div>
+      <i class="bi bi-person-badge-fill"></i>
     </div>
+
+    <div class="card">
+      <div class="card-text">
+        <h2>Usuário</h2>
+        <p>Cadastros de Usuários que irão ter acesso ao sistema</p>
+      </div>
+      <i class="bi bi-envelope"></i>
+    </div>
+
+    <div class="card">
+      <div class="card-text">
+        <h2>Área / Setor</h2>
+        <p>Cadastros de Áreas ou Setores da empresa</p>
+      </div>
+      <i class="bi bi-building"></i>
+    </div>
+
+    <div class="card">
+      <div class="card-text">
+        <h2>Grau de Risco</h2>
+        <p>Cadastros de Graus de riscos de uma anomalia</p>
+      </div>
+      <i class="bi bi-exclamation-triangle"></i>
+    </div>
+  </div>
+
 </body>
 </html>
