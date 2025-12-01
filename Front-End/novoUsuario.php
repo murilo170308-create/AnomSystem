f7d53e80b3ca72b397d67a2a8cb4cd8e273e9de6
murilo170308-create <murilo170308@gmail.com>
2025-11-30 23:57:17 -0300

    site completo

--- Front-End/novoUsuario.php
+++ Front-End/novoUsuario.php
@@ -1,3 +1,9 @@
+<?php
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -8,6 +14,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+
 
 <div class="topbar">
     <h1>Cadastrar Novo Usuário</h1>
@@ -140,8 +147,7 @@
     </script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../AnomSystem/Front-end/js/setor"></script>
-
+<script src="./js/setor.js"></script>
 
 </body>
 </html>
